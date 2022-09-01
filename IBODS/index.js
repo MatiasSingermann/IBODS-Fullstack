7ee207a8ea08c73bbff34bd8d6c79ba807b60240
MatiasSingermann <101400498+MatiasSingermann@users.noreply.github.com>
2022-09-01 15:47:41 -0300

    Menu hamburguesa funcional

--- IBODS/index.js
+++ IBODS/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const header = document.getElementById("header");
-    const hambutton = document.getElementById("div-puntos");
+    const hambutton = document.getElementById("puntos-img");
     const hammenu = document.getElementById("ham-menu-container");
     const hamcon = document.getElementById("ham-href-con");
     const hamvol = document.getElementById("ham-href-vol");
@@ -9,53 +9,18 @@ document.addEventListener("DOMContentLoaded", () => {
     const btnmenos = document.getElementById("btn-menos");
     const valorvolumen = document.getElementById("num-volumen");
 
-    hambutton.addEventListener("click", () => {
-        if(hammenu.classList.contains("show"))
-        {
-            hammenu.classList.remove("show");
-            return;
-        }
-        else
-        {
-            hammenu.classList.add("show");
-        }
-        // if (hammenu.style.display === "none") {
-        //     hammenu.style.display = "flex";
-        // }
-        // else {
-        //     hammenu.style.display = "none";
-        // }
-    }
-    );
-
-    hamcon.addEventListener("click", () => {
-        if(hammenu.classList.contains("show"))
-        {
-            hammenu.classList.remove("show");
-        }
-        // hammenu.style.display = "none";
-    }
-    );
-    hamvol.addEventListener("click", () => {
-        hammenu.classList.remove("show");
-    }
-    );
-    hamobj.addEventListener("click", () => {
-        hammenu.classList.remove("show");
-    }
-    );
-    document.addEventListener("click", (e) => {
-        console.log("Display: " + hammenu.classList);
-        if (e.target.id !== "puntos-img") {
-            hammenu.classList.remove("show");
-            return;
-        }
-
+    hambutton.addEventListener("click", (e) => {
         hammenu.classList.toggle("show");
-        
-        console.log("Target: " + e.target.id);
     }
     );
+
+    document.addEventListener("click", (e) => {
+        if(e.target !== hambutton)
+        {
+            console.log("1");
+            hammenu.classList.remove("show");
+        }
+    });
 
     btnmas.addEventListener("mousedown", () => {
         if (valorvolumen.value < 100) {
