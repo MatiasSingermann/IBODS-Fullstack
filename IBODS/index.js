3bed88f8cdf079bf4d483f4bd3b1e2db3e4eded7
MatiasSingermann <101400498+MatiasSingermann@users.noreply.github.com>
2022-09-07 15:51:21 -0300

    Mejoras en menu y volumen

--- IBODS/index.js
+++ IBODS/index.js
@@ -5,15 +5,23 @@ document.addEventListener("DOMContentLoaded", () => {
     const hamcon = document.getElementById("ham-href-con");
     const hamvol = document.getElementById("ham-href-vol");
     const hamobj = document.getElementById("ham-href-obj");
+    const blackout = document.getElementById("fondo-negro");
 
     const btnmas = document.getElementById("btn-mas");
     const btnmenos = document.getElementById("btn-menos");
     const valorvolumen = document.getElementById("num-volumen");
     const valorslider = document.getElementById("volume-slider");
 
+    const btnfondo = document.getElementById("new-checkbox-container");
+    const btntilde = document.getElementsByClassName("tilde-img");
+    const btncruz = document.getElementsByClassName("cruz-img");
+
+
+    // Menu Hamburguesa
 
     hambutton.addEventListener("click", (e) => {
         hammenu.classList.toggle("show");
+        blackout.classList.toggle("mostrar");
     }
     );
 
@@ -22,16 +30,12 @@ document.addEventListener("DOMContentLoaded", () => {
         {
             console.log("1");
             hammenu.classList.remove("show");
+            blackout.classList.remove("mostrar");
         }
     });
 
 
-    const autosumador = (e) => {
-        if (valorvolumen.value < 100) {
-            let valvol = parseInt(valorvolumen.value) + 1;
-            valorvolumen.value = valvol;
-        }
-    }
+    // Volumen
 
     btnmas.addEventListener("click", () => {
         if (valorvolumen.value < 100) {
@@ -42,6 +46,7 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log(valorvolumen.value);
     }
     );
+
     btnmenos.addEventListener("click", () => {
         if (valorvolumen.value > 0) {
             let valvol = parseInt(valorvolumen.value) - 1;
@@ -54,6 +59,30 @@ document.addEventListener("DOMContentLoaded", () => {
 
     valorslider.addEventListener("change", () => {
         valorvolumen.value = valorslider.value;
+    }
+    );
+
+    valorvolumen.addEventListener("change", () => {
+        valorslider.value = valorvolumen.value;
+
+        if(valorvolumen.value > 100){
+            valorvolumen.value = 100;
+        }
+        if(valorvolumen.value < 0){
+            valorvolumen.value = 0;
+        }
+        if(valorvolumen.value === ""){
+            valorvolumen.value = 50;
+        }
+    }
+    );
+
+
+    // Objetos
+
+    btntilde.querySelector(".tilde-img").addEventListener("click", () => {
+        btnfondo.style.backgroundColor = "rgba(1, 1, 1, 1)";
+        console.log("juan");
     }
     );
 }
